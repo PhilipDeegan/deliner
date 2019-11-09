5287c6e0ce1aa5da3678d567c8ab7ec18a653da6
dekken <philip.deegan@gmail.com>
2019-11-09 14:18:51 +0100

    constexpr + formatting

--- cpp.cpp
+++ cpp.cpp
@@ -31,160 +31,162 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "kul/io.hpp"
 #include "kul/signal.hpp"
 
-namespace deliner{
-class Exception : public kul::Exception{
-    public:
-        Exception(const char*f, const uint16_t& l, const std::string& s) : kul::Exception(f, l, s){}
+namespace deliner {
+class Exception : public kul::Exception {
+ public:
+  Exception(const char* f, const uint16_t& l, const std::string& s) : kul::Exception(f, l, s) {}
 };
 
-class Pattern{
-    private:
-        const std::string s;
-        std::string st;
-    public:
-        Pattern(const std::string& s) : s(s){
-            if(s.find("*") != std::string::npos) st = s.substr(0, s.find("*"));
-        }
-        const std::string& start() const { return st; }
-        std::string end() const {
-            if(s != st) return s.substr(st.size() + 1);
-            return "";            
-        }
-        const std::string& str() const { return s; }
+class Pattern {
+ private:
+  const std::string s;
+  std::string st;
+
+ public:
+  Pattern(const std::string& s) : s(s) {
+    if (s.find("*") != std::string::npos) st = s.substr(0, s.find("*"));
+  }
+  const std::string& start() const { return st; }
+  std::string end() const {
+    if (s != st) return s.substr(st.size() + 1);
+    return "";
+  }
+  const std::string& str() const { return s; }
 };
 
-}
+}  // namespace deliner
 
 int main(int argc, char* argv[]) {
-    kul::Signal sig;
-    const char* DIR = "dir";
-    const char* FILE = "file";
-    const char* PATTERN = "pattern";
-    const char* RECURSE = "recurse";
-    try{
-        using namespace kul::cli;
+  kul::Signal sig;
+  constexpr char* DIR = "dir";
+  constexpr char* FILE = "file";
+  constexpr char* PATTERN = "pattern";
+  constexpr char* RECURSE = "recurse";
+  try {
+    using namespace kul::cli;
 
-        std::vector<Arg> argV { Arg('d', DIR,  ArgType::STRING),
-                                Arg('f', FILE, ArgType::STRING),
-                                Arg('p', PATTERN, ArgType::STRING),
-                                Arg('r', "recurse")
-                              };
-        std::vector<Cmd> cmdV;
+    std::vector<Arg> argV{Arg('d', DIR, ArgType::STRING), Arg('f', FILE, ArgType::STRING),
+                          Arg('p', PATTERN, ArgType::STRING), Arg('r', "recurse")};
+    std::vector<Cmd> cmdV;
 
-        Args args(cmdV, argV);
-        try{
-            args.process(argc, argv);
-        }catch(const kul::cli::ArgNotFoundException& e){
-            KERR << e.stack();
-            KEXIT(0, "");
-        }
-        if(args.has(DIR)) {
-            kul::Dir d(args.get(DIR));
-            if(!d) KEXCEPT(deliner::Exception, "Directory does not exist: " + d.path());
-            kul::env::CWD(d);
-        }
-        kul::File conf(".deliner");
-        if(conf && (args.empty() || (args.size() == 1 && args.has(DIR)))){
-            kul::io::Reader reader(conf);
-            const char* c = reader.readLine();
-            std::string s = c ? c : "";
-            if(s.substr(0, 3) == "#! "){
-                std::string line(s.substr(3));
-                if(!line.empty()){
-                    std::vector<std::string> lineArgs(kul::cli::asArgs(line));
-                    std::vector<char*> lineV;
-                    for(size_t i = 0; i < lineArgs.size(); i++) lineV.push_back(&lineArgs[i][0]);
-                    return main(lineArgs.size(), &lineV[0]);
-                }
-            }
-        }
-        std::vector<std::string> types;
-        if(!args.has(FILE)) KEXCEPT(deliner::Exception, "No file type specified e.g. -f cpp,hpp");
-        kul::String::SPLIT(args.get(FILE), ',', types);
-        kul::hash::set::String fts;
-        for(const auto& s : types) fts.insert(s);
-
-        std::vector<deliner::Pattern> pats;
-        if(args.has(PATTERN)) pats.push_back(deliner::Pattern(args.get(PATTERN)));
-        if(conf){
-            kul::io::Reader r(conf);
-            const char* l = 0;
-            while((l = r.readLine())){
-                if(!strlen(l)) continue;
-                if(l[0] == '#') continue;
-                pats.push_back(deliner::Pattern(std::string(l)));
-            }
-        }
-        for(const auto& p : pats){ 
-            if(p.str()[0] == '*') KEXCEPT(deliner::Exception, "Pattern may not start with *");
-            if(p.str()[p.str().size() - 1] == '*') KEXCEPT(deliner::Exception, "Pattern may not end with *");
-            if(std::count(p.str().begin(), p.str().end(), '*') > 1) KEXCEPT(deliner::Exception, "Pattern may not contains more than one *");
-        }
-        for(const auto file : conf.dir().files(args.has(RECURSE))){
-            const std::string f = file.name();
-            if(f.rfind('.') != std::string::npos){
-                const std::string& ft(f.substr(f.rfind('.') + 1));
-                if(fts.count(ft)){
-                    kul::File tmp(file.real()+".tmp");
-                    kul::io::Writer w(tmp);
-                    kul::io::Reader r(file);
-                    std::stringstream ss;
-                    bool y = 1;
-                    const char* l = r.readLine();
-                    const deliner::Pattern* pat = 0;
-                    if(l){
-                        w << l;
-                        while((l = r.readLine())){
-                            if(!strlen(l)){
-                                w << kul::os::EOL();
-                                continue;
-                            } 
-                            y = 1;
-                            ss << l;
-                            std::string tr(l);
-                            kul::String::TRIM(tr);
-                            if(pat){
-                                ss.str(std::string());
-                                w << kul::os::EOL();
-                                const std::string& e(pat->end());
-                                if(tr.rfind(e) == tr.size() - e.size()){
-                                    pat = 0;
-                                    y = 0;
-                                    continue;
-                                }
-                            }
-                            else
-                                for(const auto& p : pats){
-                                    if(p.start().size() > 0){
-                                        if(tr.find(p.start()) == 0){
-                                            w << kul::os::EOL();
-                                            y = 0;
-                                            ss.str(std::string());
-                                            const std::string& e(p.end());
-                                            if(tr.rfind(e) != tr.size() - e.size()) pat = &p;
-                                            break;
-                                        }
-                                    }else
-                                    if(tr == p.str()){
-                                        w << kul::os::EOL();
-                                        ss.str(std::string());
-                                        y = 0;
-                                        break;
-                                    }
-                                }
-                            if(y){
-                                w << kul::os::EOL() << ss.str();
-                                ss.str(std::string());
-                            }
-                        }
-                    }
-                    file.rm();
-                    tmp.mv(file);
-                }
-            }
-        }
+    Args args(cmdV, argV);
+    try {
+      args.process(argc, argv);
+    } catch (const kul::cli::ArgNotFoundException& e) {
+      KERR << e.stack();
+      KEXIT(0, "");
     }
-    catch(const kul::Exception& e){ KERR << e.stack(); return 2;}
-    catch(const std::exception& e){ KERR << e.what(); return 3;}
-    return 0;
+    if (args.has(DIR)) {
+      kul::Dir d(args.get(DIR));
+      if (!d) KEXCEPT(deliner::Exception, "Directory does not exist: " + d.path());
+      kul::env::CWD(d);
+    }
+    kul::File conf(".deliner");
+    if (conf && (args.empty() || (args.size() == 1 && args.has(DIR)))) {
+      kul::io::Reader reader(conf);
+      const char* c = reader.readLine();
+      std::string s = c ? c : "";
+      if (s.substr(0, 3) == "#! ") {
+        std::string line(s.substr(3));
+        if (!line.empty()) {
+          std::vector<std::string> lineArgs(kul::cli::asArgs(line));
+          std::vector<char*> lineV;
+          for (size_t i = 0; i < lineArgs.size(); i++) lineV.push_back(&lineArgs[i][0]);
+          return main(lineArgs.size(), &lineV[0]);
+        }
+      }
+    }
+    std::vector<std::string> types;
+    if (!args.has(FILE)) KEXCEPT(deliner::Exception, "No file type specified e.g. -f cpp,hpp");
+    kul::String::SPLIT(args.get(FILE), ',', types);
+    kul::hash::set::String fts;
+    for (const auto& s : types) fts.insert(s);
+
+    std::vector<deliner::Pattern> pats;
+    if (args.has(PATTERN)) pats.push_back(deliner::Pattern(args.get(PATTERN)));
+    if (conf) {
+      kul::io::Reader r(conf);
+      const char* l = 0;
+      while ((l = r.readLine())) {
+        if (!strlen(l)) continue;
+        if (l[0] == '#') continue;
+        pats.push_back(deliner::Pattern(std::string(l)));
+      }
+    }
+    for (const auto& p : pats) {
+      if (p.str()[0] == '*') KEXCEPT(deliner::Exception, "Pattern may not start with *");
+      if (p.str()[p.str().size() - 1] == '*')
+        KEXCEPT(deliner::Exception, "Pattern may not end with *");
+      if (std::count(p.str().begin(), p.str().end(), '*') > 1)
+        KEXCEPT(deliner::Exception, "Pattern may not contains more than one *");
+    }
+    for (const auto file : conf.dir().files(args.has(RECURSE))) {
+      const std::string f = file.name();
+      if (f.rfind('.') != std::string::npos) {
+        const std::string& ft(f.substr(f.rfind('.') + 1));
+        if (fts.count(ft)) {
+          kul::File tmp(file.real() + ".tmp");
+          kul::io::Writer w(tmp);
+          kul::io::Reader r(file);
+          std::stringstream ss;
+          bool y = 1;
+          const char* l = r.readLine();
+          const deliner::Pattern* pat = 0;
+          if (l) {
+            w << l;
+            while ((l = r.readLine())) {
+              if (!strlen(l)) {
+                w << kul::os::EOL();
+                continue;
+              }
+              y = 1;
+              ss << l;
+              std::string tr(l);
+              kul::String::TRIM(tr);
+              if (pat) {
+                ss.str(std::string());
+                w << kul::os::EOL();
+                const std::string& e(pat->end());
+                if (tr.rfind(e) == tr.size() - e.size()) {
+                  pat = 0;
+                  y = 0;
+                  continue;
+                }
+              } else
+                for (const auto& p : pats) {
+                  if (p.start().size() > 0) {
+                    if (tr.find(p.start()) == 0) {
+                      w << kul::os::EOL();
+                      y = 0;
+                      ss.str(std::string());
+                      const std::string& e(p.end());
+                      if (tr.rfind(e) != tr.size() - e.size()) pat = &p;
+                      break;
+                    }
+                  } else if (tr == p.str()) {
+                    w << kul::os::EOL();
+                    ss.str(std::string());
+                    y = 0;
+                    break;
+                  }
+                }
+              if (y) {
+                w << kul::os::EOL() << ss.str();
+                ss.str(std::string());
+              }
+            }
+          }
+          file.rm();
+          tmp.mv(file);
+        }
+      }
+    }
+  } catch (const kul::Exception& e) {
+    KERR << e.stack();
+    return 2;
+  } catch (const std::exception& e) {
+    KERR << e.what();
+    return 3;
+  }
+  return 0;
 }
